d266f960eb8a30c3cce21ba7a748447a64dc87f4
apobekiaris <apostolis.bekiaris@gmail.com>
2016-03-05 18:43:56 +0200

    EasyTest with space in path fail

--- XpandTestExecutor/ExecutorWrapper/Program.cs
+++ XpandTestExecutor/ExecutorWrapper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 
@@ -14,13 +15,22 @@ namespace ExecutorWrapper {
         const int SW_HIDE = 0;
 
         static void Main(string[] args){
+            Trace.AutoFlush = true;
+            Trace.UseGlobalLock = false;
+            var directoryName = Path.GetDirectoryName(typeof(Program).Assembly.Location) + "";
+            var streamWriter = File.CreateText(Path.Combine(directoryName, "executorwrapper.log"));
+            Trace.Listeners.Add(new TextWriterTraceListener(streamWriter));
             var handle = GetConsoleWindow();
             ShowWindow(handle, SW_HIDE);
             var arguments = string.Join(" ", args.Skip(1));
             var debugMode = arguments.Contains("-d:");
-            var processStartInfo = new ProcessStartInfo { FileName = args[0], Arguments = arguments, UseShellExecute = debugMode, CreateNoWindow = debugMode };
+            var processStartInfo = new ProcessStartInfo { FileName = args[0], Arguments = @"""" + arguments + @"""", UseShellExecute = debugMode, CreateNoWindow = debugMode, RedirectStandardOutput = !debugMode };
             var process = new Process(){StartInfo = processStartInfo};
             process.Start();
+            if (!debugMode){
+                var readToEnd = process.StandardOutput.ReadToEnd();
+                Trace.WriteLine(readToEnd);
+            }
             process.WaitForExit();
         }
     }
