5596e3c797d00b232a9e087462cdb1b31521213a
apobekiaris <apostolis.bekiaris@gmail.com>
2017-01-22 17:48:54 +0200

    Handle LogErros exceptions in TestExecutor

--- XpandTestExecutor/XpandTestExecutor.Module/Services/TestRunner.cs
+++ XpandTestExecutor/XpandTestExecutor.Module/Services/TestRunner.cs
@@ -89,6 +89,19 @@ namespace XpandTestExecutor.Module.Services {
         private static void LogErrors(EasyTest easyTest, Exception e) {
             lock (_locker) {
                 Tracing.Tracer.LogSeparator("LogErrors");
+                Tracing.Tracer.LogError(e);
+                LogErrorsCore(easyTest, e);
+            }
+
+        }
+
+        private static void LogErrorsCore(EasyTest easyTest, Exception e) {
+            Tracing.Tracer.LogSeparator("LogErrorsCore");
+            try {
+                easyTest.LastEasyTestExecutionInfo.Update(EasyTestState.Failed);
+                easyTest.LastEasyTestExecutionInfo.Setup(true);
+                easyTest.Session.ValidateAndCommitChanges();
+                EnviromentEx.LogOffUser(easyTest.LastEasyTestExecutionInfo.WindowsUser.Name);
                 var directoryName = Path.GetDirectoryName(easyTest.FileName) + "";
                 var logTests = new LogTests();
                 foreach (var application in easyTest.Options.Applications.Cast<TestApplication>()) {
@@ -98,16 +111,13 @@ namespace XpandTestExecutor.Module.Services {
                     logTests.Tests.Add(logTest);
                 }
                 logTests.Save(Path.Combine(directoryName, "TestsLog.xml"));
-                EnviromentEx.LogOffUser(easyTest.LastEasyTestExecutionInfo.WindowsUser.Name);
-                
-                easyTest.LastEasyTestExecutionInfo.Update(EasyTestState.Failed);
-                easyTest.LastEasyTestExecutionInfo.Setup(true);
-                easyTest.Session.ValidateAndCommitChanges();
-                Tracing.Tracer.LogError(e);
-            }
-            
-        }
 
+            }
+            catch (Exception exception) {
+                Tracing.Tracer.LogError(exception);
+                throw;
+            }
+        }
         private static void AfterProcessExecute(IDataLayer dataLayer, Guid easyTestKey) {
             lock (_locker) {
                 using (var unitOfWork = new UnitOfWork(dataLayer)) {
@@ -139,7 +149,7 @@ namespace XpandTestExecutor.Module.Services {
             string fileName = Path.Combine(directoryName, "config.xml");
             using (var optionsStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read)) {
                 Options options = Options.LoadOptions(optionsStream, null, null, directoryName);
-                foreach (var alias in options.Aliases.Cast<TestAlias>().Where(@alias => alias.ContainsAppPath())) {
+                foreach (var alias in options.Aliases.Cast<TestAlias>().Where(alias => alias.ContainsAppPath())) {
                     var suffix = alias.IsWinAppPath() ? "_win" : "_web";
                     var sourceFileName = Path.Combine(alias.Value, "eXpressAppFramework.log");
                     if (File.Exists(sourceFileName)) {
